e78d13f087552aad8ed211a25a56ae9806b5ba99
gherardovarando <gherardo.varand@gmail.com>
2019-07-18 11:49:03 +0200

    [f] tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,19 +1,5 @@
 context("Utility functions")
 
-test_that("rHurwitz produce stable matrices", {
-  expect_true(all(replicate(100, {
-    B <- rHurwitz(p = sample(1:50, size = 1))
-    all(Re(eigen(B)$values) < 0)
-  })))
-})
-
-test_that("rStableMetzler produce stable matrices", {
-  expect_true(all(replicate(100, {
-    B <- rStableMetzler(p = sample(1:50, size = 1), d = runif(1))
-    all(Re(eigen(B)$values) < 0)
-  })))
-})
-
 test_that("minus log likelihood is minimized by empirical covariance matrix", {
   B <- rHurwitz(p = 10)
   M <- gmat::chol_mh(N = 1000, p = 10)
