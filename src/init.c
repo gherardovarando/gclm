b546182bdf91a2b9324d78a2532292d8eba8bf42
gherardovarando <gherardo.varando@gmail.com>
2020-02-19 14:20:26 +0100

    [f]

--- src/init.c
+++ src/init.c
@@ -9,14 +9,14 @@
 /* .Fortran calls */
 extern void F77_NAME(dgelyp)(void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(grddsllc)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-extern void F77_NAME(pnllbc)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(pnllbc)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(prxgrdllb)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(prxgrdlsb)(void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_FortranMethodDef FortranEntries[] = {
     {"dgelyp",    (DL_FUNC) &F77_NAME(dgelyp),     8},
     {"grddsllc",  (DL_FUNC) &F77_NAME(grddsllc),  12},
-    {"pnllbc",    (DL_FUNC) &F77_NAME(pnllbc),    13},
+    {"pnllbc",    (DL_FUNC) &F77_NAME(pnllbc),    12},
     {"prxgrdllb", (DL_FUNC) &F77_NAME(prxgrdllb),  10},
     {"prxgrdlsb", (DL_FUNC) &F77_NAME(prxgrdlsb),  9},
     {NULL, NULL, 0}
