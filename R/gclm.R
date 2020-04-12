5846f0663a95c3f1703ee446553aebe43198d680
gherardovarando <gherardo.varand@gmail.com>
2020-04-12 08:07:59 +0200

    reformat code + default lambda seq

--- R/gclm.R
+++ R/gclm.R
@@ -130,3 +130,74 @@ gclm <- function(Sigma, B = - 0.5 * diag(ncol(Sigma)),
   out$B <- matrix(nrow = out$N, out$B)
   return(out)
 }
+
+
+#' @rdname gclm
+#' @param lambdas sequence of lambda
+#' @param ... additional arguments passed to \code{gclm}
+#' @export
+gclm.path <- function(Sigma, lambdas = NULL, 
+                      B = - 0.5 * diag(ncol(Sigma)),
+                      C = rep(1, ncol(Sigma)),
+                      ...){
+  if (is.null(lambdas)) {
+    lambdas = seq(max(diag(Sigma)) / 2, 0, length = 10)
+  }
+  results <- list()
+  for (i in 1:length(lambdas)){
+    results[[i]] <- gclm(Sigma, B = B, C = C,
+                         lambda = lambdas[i],  ...)
+    B <- results[[i]]$B
+    C <- results[[i]]$C
+  }
+  return(results)
+}
+
+
+#' Recover lower triangular GCLM
+#'
+#' Recover the only lower triangular stable matrix B such that 
+#' \code{Sigma} (\eqn{\Sigma})
+#' is the solution of the associated continuous Lyapunov equation:
+#' \deqn{B\Sigma + \Sigma B' + C = 0}
+#'
+#'
+#' @param Sigma covariance matrix
+#' @param P the inverse of the  covariance matrix
+#' @param C symmetric positive definite matrix 
+#'
+#' @return A stable lower triangular matrix
+#'
+#' @export
+gclm.lowertri <- function(Sigma,
+                          P = solve(Sigma),
+                          C = diag(nrow = nrow(Sigma))){
+  
+  p <- nrow(Sigma)
+  l <- listInverseBlocks(Sigma)
+  blong <- anti_t(0.5 * C %*% P)[ upper.tri(Sigma)]
+  blong <- blong[length(blong):1]
+  b <- blong[1:(p-1)]
+  w <- l[[1]] %*% b
+  t <- p
+  for (i in 2:length(l)){
+    b <- blong[t:(t + p - i - 1)]
+    t <- t + p - i
+    AA <- matrix(nrow = length(b), ncol = length(w), 0)
+    for (j in (i+1):p){
+      for (k in 1:(i-1)){
+        ix <- (k - 1) * p - (k - 1) * k / 2 + (i - k)
+        AA[j - i, ix] <-  P[k, j]
+      }
+    }
+    b <- b + tcrossprod(AA, t(w) )
+    w <- c(w, l[[i]] %*% b)
+  }
+  W <- matrix(nrow = p, ncol = p, 0)
+  W[upper.tri(W)] <- w[length(w) : 1]
+  W <- anti_t(W)
+  W <- W - t(W)
+  B <- (W - 0.5 * C) %*% P
+  B[upper.tri(B)] <- 0
+  return(B)
+}
